55b66b225b9e0274020f092e28fd98cfbd249570
Rafael Nicolett <rafaelnicolett@outlook.com>
2016-12-16 17:00:24 -0200

    Initial Commit

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Rafael Nicoleti on 16/12/16.
+//  Copyright © 2016 Rafael Nicoleti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
